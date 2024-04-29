b12a9e27302e71b98e7d8b004e0f0eb953763f5c
jazz7y <165008584+jazz7y@users.noreply.github.com>
2024-04-29 17:54:41 +0500

    Add files via upload

---
+++ Webber.lua - Main.lua
@@ -0,0 +1,5 @@
+script:name("Dowloader - webber.lua") -- Имя скрипта
+script:devs({"||| by: jazzy", "ds: jazzy.web, server: dsc.gg/-pon"})
+script:desc("Example Script") -- Описание
+
+dowloader = module.new("Dowloader - webber.lua", "attackstrafe module for webber.lua | bypass: vanila, matix, infinityhvh")
